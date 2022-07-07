c13bcb4ecd3abf8552251391fd76f0096cb89655
Dominik1123 <>
2022-07-07 22:12:22 +0200

    Fix atomic weight fetching

--- acc-companion/src/acc_companion/resources/fetch_atomic_weights.py
+++ acc-companion/src/acc_companion/resources/fetch_atomic_weights.py
@@ -2,53 +2,49 @@
    mapping symbols to atomic weights per nucleon in MeV.
 """
 
-import ast
+from collections import defaultdict
 import json
 from pathlib import Path
 import re
+from urllib.error import HTTPError
 
 import pandas as pd
 from scipy.constants import physical_constants
 
 
-URL = 'https://ciaaw.org/atomic-weights.htm'
+URL = 'https://ciaaw.org'
+AMU_MEV = physical_constants['atomic mass constant energy equivalent in MeV'][0]
+AVAILABLE = '\u2714'
+NOT_AVAILABLE = '\u2718'
 
 
 def parse_atomic_weight(s):
-    if s == '—':
-        return None
     s = re.sub(r'\s', '', s)
-    s = re.sub(r'\(\d+\)', '', s)
-    v = ast.literal_eval(s)
-    if isinstance(v, float):
-        return v
-    elif isinstance(v, list):
-        assert len(v) == 2
-        return (v[0] + v[1]) / 2
+    m = re.fullmatch(r'([1-9][0-9]*(?:[.][0-9]+)?)\((?:exact|[0-9])\)', s)
+    return float(m.group(1))
+
+
+def parse_isotope(s):
+    s = re.sub(r'\s', '', s)
+    m = re.fullmatch('([1-9][0-9]*)([A-Z][a-z]*)', s)
+    return m.group(2), int(m.group(1))
+
+
+data = defaultdict(dict)
+for element in pd.read_html(f'{URL}/atomic-weights.htm')[0]['Element']:
+    print(f'{element}', end=' ', flush=True)
+    try:
+        df, = pd.read_html(f'{URL}/{element}.htm')
+    except HTTPError as err:
+        if err.code != 404:
+            raise
+        print(NOT_AVAILABLE)
     else:
-        assert False, type(v)
-
-
-df = pd.read_html(URL)[0].iloc[:-1]
-z_per_symbol = {
-    s: int(z)
-    for s, z in df.set_index('Symbol')['Z'].to_dict().items()
-}
-weight_per_symbol = {
-    s: parse_atomic_weight(w)
-    for s, w in df.set_index('Symbol')['Standard Atomic Weight'].to_dict().items()
-}
-data = {
-    symbol: (
-        weight
-        * physical_constants['atomic mass constant energy equivalent in MeV'][0]
-        / (2 * z_per_symbol[symbol])
-    )
-    for symbol, weight in weight_per_symbol.items()
-    if weight is not None
-}
-data['H'] *= 2  # correct for wrong division by 2 above
+        for __, (isotope, atomic_mass) in df.loc[:,['Isotope', 'Atomic mass (Da)']].iterrows():
+            element, number_of_nucleons = parse_isotope(isotope)
+            data[element][number_of_nucleons] = AMU_MEV * parse_atomic_weight(atomic_mass) / number_of_nucleons
+        print(AVAILABLE)
 
 save_path = Path(__file__).with_name('atomic_weights.json')
 with open(save_path, 'w') as fh:
-    json.dump(data, fh, indent=2)
+    json.dump(dict(data), fh, indent=2)
